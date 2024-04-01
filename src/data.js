c1f753f63f7a0082e7ee1640ee6a4b026dc50192
AARON <abinoy@ualberta.ca>
2024-03-31 20:28:36 -0600

    content update

--- src/data.js
+++ src/data.js
@@ -1,54 +1,67 @@
-import qrky from "./images/qrky.jpg";
-import board from "./images/board.png";
-import work1 from "./images/work01-hover.jpg"
-import work3 from "./images/work03-hover.jpg"
-import work4 from "./images/work04-hover.jpg"
-import work5 from "./images/work05-hover.jpg"
-import work6 from "./images/work06-hover.jpg"
-import work7 from "./images/bulk.png"
 export const Projects = [
     {
       id: 1,
-      name: "QRKY",
-      description: "A QR code-based inventory management system for Android.",
-      techs: "Figma, Node.js, Android Studio, Java, Google Firebase",
-      github: "https://github.com/CMPUT301W23T05/supreme-waddle",
-    },
-    {
-      id: 2,
       name: "Ninuki Agent",
-      description: "An AI agent implemented with Monte Carlo Tree Search for the Ninuki-Renju game.",
+      description: "The game + agent for a version of Gomoku, where five in a row and 10 captures win. Implemented using the Monte Carlo Tree Search algorithm.",
       techs: "Python",
       github: "https://github.com/aarongeo1/MCTS_Agent_Ninuki",
     },
     {
+      id: 2,
+      name: "Naive Bayes Text Classification",
+      description: "A text classification system using the Naive Bayes algorithm, focusing on relation extraction from textual data for natural language processing tasks.",
+      techs: "Python",
+      github: "https://github.com/aarongeo1/Naive-Bayes-Text-Classification",
+    },  
+    {
       id: 3,
+      name: "QRKY",
+      description: "A Fun game of collecting QR codes like Poke***! for Android",
+      techs: "Figma, Node.js, Android Studio, Java, Google Firebase",
+      github: "https://github.com/CMPUT301W23T05/supreme-waddle",
+    },
+    {
+      id: 4,
+      name: "N-gram-Language-Model",
+      description: "N-gram language modeling tool using Python",
+      techs: "Python",
+      github: "https://github.com/aarongeo1/N-gram-Language-Model", 
+    },
+    {
+      id: 5,
+      name: "Speech-Processing-Automation",
+      description: "A Python-based toolkit for automating the conversion and cleaning of speech transcript files, enhancing the accuracy and efficiency of linguistic data processing.",
+      techs: "Python, Cadence",
+      github: "https://github.com/aarongeo1/Speech-Processing-Automation",
+    },
+    {
+      id: 6,
+      name: "4TOE Bot",
+      description: "A tactical operations AI engine for turn-based strategy game of 4TOE",
+      techs: "Python",
+      github: "https://github.com/aarongeo1/4TOE",
+    },
+    {
+      id: 7,
       name: "BulkBarter",
-      description: "A platform for bulk trading items, aiming at reducing waste.",
+      description: "A platform for bulk trading items, aiming at reducing waste and promoting sustainable consumption through a community-driven exchange system.",
       techs: "JavaScript, React, HTML, CSS",
       github: "https://github.com/sankalpsaini/bulkbarter",
     },
     {
-      id: 4,
+      id: 8,
       name: "Edmonton Nav",
-      description: "A pathfinding application for Edmonton's public transit system.",
+      description: "A pathfinding application for Edmonton's public transit system, facilitating efficient navigation through complex urban environments.",
       techs: "C++",
       github: "https://github.com/aarongeo1/path-finder",
     },
     {
-      id: 5,
+      id: 9,
       name: "Oasis",
-      description: "A web application for remote teams to interact in a virtual office space.",
+      description: "A web application for remote teams to interact..",
       techs: "Python, Jinja, Docker",
       github: "https://github.com/aarongeo1/Oasis",
-    },
-    {
-      id: 6,
-      name: "4TOE",
-      description: "A tactical operations engine for turn-based strategy games.",
-      techs: "Python",
-      github: "https://github.com/aarongeo1/4TOE",
     }
+
   ];
   
-
